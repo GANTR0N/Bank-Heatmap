cc51f1df37236a9d55e3d1d65830b80b472313a7
sdrupa <sdrupa@vt.edu>
2022-11-26 15:44:12 -0500

    Updated files, copied over previous work by raiyni and fixed some version issues

---
+++ src/main/java/com/heatmap/HeatmapPlugin.java
@@ -0,0 +1,209 @@
+package com.heatmap;
+
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Provides;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.events.ScriptPostFired;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetID;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Bank Heatmap"
+)
+public class HeatmapPlugin extends Plugin
+{
+	enum HEATMAP_MODE
+	{
+		NULL,
+		HA,
+		GE
+	}
+
+	private static final List<Integer> TAB_VARBITS = ImmutableList.of(
+		Varbits.BANK_TAB_ONE_COUNT,
+		Varbits.BANK_TAB_TWO_COUNT,
+		Varbits.BANK_TAB_THREE_COUNT,
+		Varbits.BANK_TAB_FOUR_COUNT,
+		Varbits.BANK_TAB_FIVE_COUNT,
+		Varbits.BANK_TAB_SIX_COUNT,
+		Varbits.BANK_TAB_SEVEN_COUNT,
+		Varbits.BANK_TAB_EIGHT_COUNT,
+		Varbits.BANK_TAB_NINE_COUNT
+	);
+
+	@Inject
+	private Client client;
+
+	@Inject
+	private HeatmapCalculation heatmapCalculation;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Inject
+	private HeatmapItemOverlay heatmapItemOverlay;
+
+	@Inject
+	private HeatmapTutorialOverlay heatmapTutorialOverlay;
+
+	@Inject
+	private HeatmapConfig config;
+
+	@Getter
+	private HEATMAP_MODE heatmapMode = HEATMAP_MODE.NULL;
+
+	@Override
+	protected void startUp()
+	{
+		overlayManager.add(heatmapItemOverlay);
+		overlayManager.add(heatmapTutorialOverlay);
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		overlayManager.remove(heatmapItemOverlay);
+		overlayManager.remove(heatmapTutorialOverlay);
+		heatmapMode = HEATMAP_MODE.NULL;
+	}
+
+	@Provides
+	HeatmapConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(HeatmapConfig.class);
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (!"heatmap".equals(event.getGroup()) || !"tutorial".equals(event.getKey()))
+		{
+			return;
+		}
+
+		if (config.showTutorial())
+		{
+			overlayManager.add(heatmapTutorialOverlay);
+		}
+		else
+		{
+			overlayManager.remove(heatmapTutorialOverlay);
+		}
+	}
+
+	@Subscribe
+	public void onScriptPostFired(ScriptPostFired event)
+	{
+		if (event.getScriptId() == ScriptID.BANKMAIN_BUILD)
+		{
+			Item[] items = getBankTabItems();
+			heatmapItemOverlay.getHeatmapImages().invalidateAll();
+			heatmapCalculation.calculate(items);
+		}
+	}
+
+	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event)
+	{
+		if (event.getType() != MenuAction.CC_OP.getId() || !event.getOption().equals("Show menu")
+			|| (event.getActionParam1() >> 16) != WidgetID.BANK_GROUP_ID)
+		{
+			return;
+		}
+
+		client.createMenuEntry(-1)
+			.setOption("Toggle GE Heatmap")
+			.setTarget("")
+			.setType(MenuAction.RUNELITE)
+			.onClick(this::onClick)
+			.setDeprioritized(true);
+
+		client.createMenuEntry(-1)
+			.setOption("Toggle HA Heatmap")
+			.setTarget("")
+			.setType(MenuAction.RUNELITE)
+			.onClick(this::onClick)
+			.setDeprioritized(true);
+
+
+		if (config.showTutorial())
+		{
+			client.createMenuEntry(-1)
+				.setOption("Disable tutorial")
+				.setTarget("")
+				.setType(MenuAction.WIDGET_FIFTH_OPTION)
+				.setIdentifier(event.getIdentifier())
+				.setParam0(event.getActionParam0())
+				.setParam1(event.getActionParam1())
+				.onClick(e -> config.setTutorial(false))
+				.setDeprioritized(true);
+		}
+	}
+
+	public void onClick(MenuEntry e)
+	{
+		HEATMAP_MODE mode = e.getOption().equals("Toggle GE Heatmap") ? HEATMAP_MODE.GE : HEATMAP_MODE.HA;
+		if (mode == heatmapMode)
+		{
+			heatmapMode = HEATMAP_MODE.NULL;
+		}
+		else
+		{
+			heatmapItemOverlay.getHeatmapImages().invalidateAll();
+			heatmapMode = mode;
+		}
+	}
+
+	private Item[] getBankTabItems()
+	{
+		final ItemContainer container = client.getItemContainer(InventoryID.BANK);
+		if (container == null)
+		{
+			return null;
+		}
+
+		final Item[] items = container.getItems();
+		int currentTab = client.getVarbitValue(Varbits.CURRENT_BANK_TAB);
+
+		if (currentTab > 0)
+		{
+			int startIndex = 0;
+
+			for (int i = currentTab - 1; i > 0; i--)
+			{
+				startIndex += client.getVarbitValue(TAB_VARBITS.get(i - 1));
+			}
+
+			int itemCount = client.getVarbitValue(TAB_VARBITS.get(currentTab - 1));
+			return Arrays.copyOfRange(items, startIndex, startIndex + itemCount);
+		}
+
+		return items;
+	}
+
+	HeatmapItem getHeatmapItem(int id)
+	{
+		return heatmapCalculation.getHeatmapItems().get(id);
+	}
+
+	boolean isBankVisible()
+	{
+		Widget bank = client.getWidget(WidgetInfo.BANK_CONTAINER);
+		return config.showTutorial() && bank != null && !bank.isHidden();
+	}
+}
---
+++ src/main/java/com/heatmap/HeatmapTutorialOverlay.java
@@ -0,0 +1,62 @@
+package com.heatmap;
+
+import net.runelite.api.Client;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
+import net.runelite.client.ui.ColorScheme;
+import net.runelite.client.ui.overlay.OverlayLayer;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class HeatmapTutorialOverlay extends OverlayPanel
+{
+	private final Client client;
+	private final HeatmapPlugin plugin;
+
+	@Inject
+	private HeatmapTutorialOverlay(Client client, HeatmapPlugin plugin)
+	{
+		super(plugin);
+		setPosition(OverlayPosition.DYNAMIC);
+		setLayer(OverlayLayer.ABOVE_WIDGETS);
+		this.client = client;
+		this.plugin = plugin;
+	}
+
+	@Override
+	public Dimension render(Graphics2D graphics)
+	{
+		if (!plugin.isBankVisible())
+		{
+			return null;
+		}
+
+		Widget button = client.getWidget(WidgetInfo.BANK_SETTINGS_BUTTON);
+		if (button == null || button.isSelfHidden() || button.getDynamicChildren()[0].getSpriteId() != 195)
+		{
+			return null;
+		}
+
+		Rectangle bounds = button.getBounds();
+
+		graphics.setColor(ColorScheme.BRAND_ORANGE);
+		graphics.setStroke(new BasicStroke(2));
+		graphics.drawRect(bounds.x, bounds.y, bounds.width, bounds.height);
+
+		FontMetrics font = graphics.getFontMetrics();
+		int width = font.stringWidth("Right click this button");
+
+		graphics.setColor(ColorScheme.DARKER_GRAY_COLOR);
+		graphics.fillRect(bounds.x + bounds.width + 2, bounds.y - 15, width + 6, 30);
+
+
+		graphics.setColor(ColorScheme.BRAND_ORANGE);
+		graphics.drawString("Right click this button", bounds.x + bounds.width + 5, bounds.y);
+		graphics.drawString("for Heatmap overlay", bounds.x + bounds.width + 5, bounds.y + 12);
+
+		return super.render(graphics);
+	}
+}
